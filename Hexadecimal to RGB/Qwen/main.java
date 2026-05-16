915ea5cc4215a49c1573c991a22fa3ab5fb48f6a
pietrohs-rgb <pietro.hs@estudante.iftm.edu.br>
2026-05-16 18:00:54 -0300

    Create main.java

---
+++ Hexadecimal to RGB/Qwen/main.java
@@ -0,0 +1,10 @@
+//O SonarQube não encontrou problema algum com este codigo.
+
+def hex_to_rgb(hex_str):
+    hex_str = hex_str.lstrip('#')
+    if len(hex_str) == 3:
+        hex_str = ''.join([c * 2 for c in hex_str])
+    r = int(hex_str[0:2], 16)
+    g = int(hex_str[2:4], 16)
+    b = int(hex_str[4:6], 16)
+    return (r, g, b)
