47896d8235d7e814ce6fcea861a9bc905311f0a7
Miguel Gontijo <miguel.gontijo@estudante.iftm.edu.br>
2026-05-16 16:20:57 -0300

    Add files via upload

---
+++ How to add two numbers/gpt/Main.java
@@ -0,0 +1,11 @@
+//SonarQube disse para trocar System.out.println por Logger.
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
---
+++ How to add two numbers/llama/Main.java
@@ -0,0 +1,14 @@
+//SonarQube disse para trocar o System.out.println por um logger
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 10;  // first number
+        int num2 = 20;  // second number
+
+        // Add the two numbers
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
