835d314c96c796a0e2c429e47f9c0c1d0b3e0a3c
pietrohs-rgb <pietro.hs@estudante.iftm.edu.br>
2026-05-17 18:31:12 -0300

    Create main.java

---
+++ fibonacci sequence/lhama/main.java
@@ -0,0 +1,19 @@
+// O SonarQube aconcelhou que trocasse o System.out por Logger.
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10; // quantidade de números da sequência
+        int[] fib = new int[n];
+
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib[i] + " ");
+        }
+    }
+}
