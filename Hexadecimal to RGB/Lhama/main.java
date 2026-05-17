1f4e189bde4e9635ca3c68d860d37bb089792148
pietrohs-rgb <pietro.hs@estudante.iftm.edu.br>
2026-05-17 18:16:34 -0300

    Update and rename main.py to main.java

---
+++ Hexadecimal to RGB/Lhama/main.java
@@ -0,0 +1,23 @@
+// O SonarQube sugeriu:
+// falou para muidar o file para uma "named package".
+// falou que O arquivo quary1.java não faz parte do projeto; apenas erros de sintaxe são relatados.
+// falou para Mover os designadores de matriz [] para o tipo. em relação ao int RGB.
+// E disse para trocar o "sistem.out" para um "logger".
+public class HexToRgb {
+    public static void main(String[] args) {
+        String hex = "#FF0000";
+        int rgb[] = hexToRgb(hex);
+        System.out.println("RGB: (" + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ")");
+    }
+
+    public static int[] hexToRgb(String hex) {
+        int[] rgb = new int[3];
+        hex = hex.replace("#", "");
+        rgb[0] = Integer.parseInt(hex.substring(0, 2), 16);
+        rgb[1] = Integer.parseInt(hex.substring(2, 4), 16);
+        rgb[2] = Integer.parseInt(hex.substring(4, 6), 16);
+        return rgb;
+    }
+
+    
+}
